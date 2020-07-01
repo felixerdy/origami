2343764fe04d346ea7aa08600cea9383055527ae
Yousef <yousef@gmx.de>
2020-07-01 19:18:03 +0200

    #197: update scanning results in view

--- src/app/pages/ibeacon/ibeacon.page.ts
+++ src/app/pages/ibeacon/ibeacon.page.ts
@@ -1,3 +1,6 @@
+import { Component, ChangeDetectorRef } from '@angular/core';
+  constructor(private readonly ibeacon: IBeacon, private readonly platform: Platform, private changeRef: ChangeDetectorRef) {
+    this.platform.ready().then(() => {
   public stopScannning(): void {
     // stop ranging
     this.ibeacon.stopRangingBeaconsInRegion(this.beaconRegion)
@@ -8,3 +11,6 @@
         console.log(`Failed to stop ranging beacon region: `, this.beaconRegion);
       });
   }
+
+            this.changeRef.detectChanges(); // Check for data change to update view Y.Q
+          } else {
